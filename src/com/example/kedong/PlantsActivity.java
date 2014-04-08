6b83dc83dc685d3f0d6fb7ea1db18651378bdc98
jsjdxysuper <jsjdxysuper@126.com>
2014-04-08 06:46:40 +0000

    能够正常运行的版本

--- src/com/example/kedong/PlantsActivity.java
+++ src/com/example/kedong/PlantsActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import android.os.Build;
 
 public class PlantsActivity extends Activity {
@@ -20,7 +21,34 @@ public class PlantsActivity extends Activity {
 
 
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu){
+		super.onCreateOptionsMenu(menu);
+		CreateMenu(menu);
+		return true;
+	}
 
-
-
+	private void CreateMenu(Menu menu){
+		MenuItem mnu1 = menu.add(0, 0, 0, "Item 1");
+		{
+			mnu1.setIcon(R.drawable.refresh);
+			mnu1.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+		}
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		return MenuChoice(item);
+	}
+	
+	private boolean MenuChoice(MenuItem item){
+		switch(item.getItemId()){
+		case 0:
+			Toast.makeText(this, "You Refresh the Data",
+					Toast.LENGTH_SHORT).show();
+			return true;
+		}
+		return false;
+	}
 }
